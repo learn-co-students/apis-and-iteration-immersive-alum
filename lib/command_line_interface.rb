0cf58e686faa80b5a0fe1fc6058af42c1521e0d3
Jane Costa <yberdysh@gmail.com>
2018-06-27 22:58:43 -0400

    Done.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,15 @@
 def welcome
   # puts out a welcome message here!
+  puts "Welcome to my app!"
+  puts "+++++++++++++++++++++++++++++++"
 end
 
 def get_character_from_user
+
   puts "please enter a character"
+  input = gets.chomp
+  puts "Movies with #{input}:"
+  puts "+++++++++++++++++++++++++++++++"
+  input
   # use gets to capture the user's input. This method should return that input, downcased.
 end
